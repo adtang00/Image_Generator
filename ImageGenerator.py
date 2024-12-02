384f0b3a0436c40a31472a744377f08c8f6dcd5c
adtang00 <adtang00@gmail.com>
2024-12-01 21:11:14 -0500

    12/1 Commit

--- ImageGenerator.py
+++ ImageGenerator.py
@@ -3,82 +3,151 @@ import tensorflow as tf
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
+from tensorflow.keras import Model
 from keras import Sequential
 from keras.initializers import RandomNormal
 from tensorflow import keras
 from tensorflow.keras.layers import Conv2D, Dense, Flatten, Reshape, LeakyReLU, Dropout, UpSampling2D, Conv2DTranspose
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.models import Model
+from tensorflow.keras.losses import BinaryCrossentropy
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+import os
 
-def load_dataset():
-    #ds[0] is pixels, ds[1] are the labels
-    ds = tfds.load('oxford_iiit_pet', with_info = True, as_supervised= True, split = 'train')
-    ds = ds[0].map(preprocess_images)
-    ds = ds.filter(lambda img, lbl: img is not None)
+def load_dataset(batch_size = 32):
+    # Load the dataset with images and labels (as_supervised=True gives (image, label) tuples)
+    ds = tfds.load('mnist', as_supervised=False, split='train')
+    # Map the preprocessing function to the images (this applies only to the images)
+    ds = ds.map(preprocess_images)
+
+    # Cache, shuffle, batch, and prefetch the dataset
     ds = ds.cache()
-    ds = ds.shuffle(60000)
-    ds = ds.batch(128)
-    ds = ds.prefetch(64)    #reduces bottlenecking 
-    #print(ds.as_numpy_iterator().next()[1])
+    ds = ds.shuffle(buffer_size = 6000)  # Shuffle the dataset
+    ds = ds.batch(batch_size)  # Batch the data
+    ds = ds.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)  # Prefetch to avoid bottlenecking during training
+    return ds
 
-def preprocess_images(img, label):
+def preprocess_images(img):
     try:
-        img = tf.image.resize(img, [256, 256])
-        img = tf.image.central_crop(img, central_fraction=0.875)
-        return tf.cast(img, tf.float32) / 255.0, label
+        img = tf.image.resize(img['image'], [28, 28])
+        #img = tf.image.central_crop(img, central_fraction=0.875)
+        return tf.cast(img, tf.float32) / 255.0
     except tf.errors.InvalidArgumentError:
-        return None, None
+        return None
 
-def build_generator():
+def build_generator(latent_dim=100):
     model = Sequential()
-    #takes in random values and reshapes to 7x7x128 
-    model.add(keras.layers.Dense(7*7*512, input_dim = 128, kernel_initializer=RandomNormal(stddev=0.02) ))
+
+    # Start with a (7, 7, 128) feature map after the Dense layer
+    model.add(Dense(7*7*128, input_dim=latent_dim, kernel_initializer=RandomNormal(stddev = 0.02)))
     model.add(LeakyReLU(0.2))
-    model.add(Reshape((7, 7, 512)))
+    model.add(Reshape((7, 7, 128)))  # Reshape into a 7x7x128 feature map
 
-    # Conv2DTranspose Block 1
-    model.add(keras.layers.Conv2DTranspose(256, kernel_size=(4, 4), strides=(2, 2), padding='same'))  # Upsample to 14x14
-    model.add(keras.layers.LeakyReLU(0.2))
+    # Upsample to 14x14
+    model.add(Conv2DTranspose(128, kernel_size=4, strides=2, padding='same'))
+    model.add(LeakyReLU(0.2))
 
-    # Conv2DTranspose Block 2
-    model.add(keras.layers.Conv2DTranspose(128, kernel_size=(4, 4), strides=(2, 2), padding='same'))  # Upsample to 28x28
-    model.add(keras.layers.LeakyReLU(0.2))
-    
-    # Conv2DTranspose Block 3
-    model.add(keras.layers.Conv2DTranspose(128, kernel_size=(4, 4), strides=(2, 2), padding='same'))  # Upsample to 56x56
-    model.add(keras.layers.LeakyReLU(0.2))
-
-    # Conv2DTranspose Block 4
-    model.add(keras.layers.Conv2DTranspose(64, kernel_size=(4, 4), strides=(2, 2), padding='same'))  # Upsample to 112x112
-    model.add(keras.layers.LeakyReLU(0.2))
-
-    # Final Conv2DTranspose to reach 224x224
-    model.add(keras.layers.Conv2DTranspose(3, kernel_size=(4, 4), strides=(2, 2), padding='same', activation='sigmoid'))  # 3 channels for RGB
+    # Upsample to 28x28
+    model.add(Conv2DTranspose(128, kernel_size=4, strides=2, padding='same'))  
+    model.add(LeakyReLU(0.2))
+    model.add(Conv2D(1, (7,7), activation='sigmoid', padding='same'))
     return model
 
-def build_discriminator():
+#input shape matches output of generator
+def build_discriminator(input_shape = (28, 28, 1)):
     model = Sequential()
-    model.add(Conv2D(16, 3, input_shape = (224, 224, 3)))
+    model.add(Conv2D(64, kernel_size = 3,strides = 2, input_shape = input_shape, padding = 'same'))
+    #model.add(keras.layers.BatchNormalization())
     model.add(LeakyReLU(0.2))
-    model.add(Dropout(0.4))
+    model.add(Dropout(0.3))
 
-    model.add(Conv2D(32, 3))
+    model.add(Conv2D(64, kernel_size = 3,strides = 2, padding = 'same'))
+    #model.add(keras.layers.BatchNormalization())
     model.add(LeakyReLU(0.2))
-    model.add(Dropout(0.4))
-
-    model.add(Conv2D(64, 3))
-    model.add(LeakyReLU(0.2))
-    model.add(Dropout(0.4))
-
-    #model.add(Conv2D(128, 5))
-    #model.add(LeakyReLU(0.2))
-    #model.add(Dropout(0.4))
+    model.add(Dropout(0.3))
 
     model.add(Flatten())
-    model.add(Dropout(0.4))
+    model.add(Dropout(0.3))
     model.add(Dense(1, activation='sigmoid'))
     return model
 
-def training_loop():
-    pass
+class image_generator(tf.keras.Model):
+    def __init__(self, generator, discriminator, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        #Create attributes for gen and disc
+        self.generator = generator
+        self.discriminator = discriminator
+
+    def compile(self,*args, **kwargs):
+        #Compile with base class
+        #Adam as optimizer, BinaryCrossEntropy as loss function
+        super().compile(*args, **kwargs)
+        self.g_opt = Adam(learning_rate = 0.0002, beta_1=0.5)
+        self.d_opt = Adam(learning_rate = 0.0002, beta_1=0.5)
+        self.g_loss = BinaryCrossentropy()
+        self.d_loss = BinaryCrossentropy()
+
+    def train_step(self, image_batch, batch_size = 32):
+        real_images = image_batch
+        fake_images = self.generator(tf.random.normal((batch_size, 100, 1)), training = False)
+        
+        #train discriminator
+        with tf.GradientTape() as d_tape:
+            #pass the real and fake images to the discriminator model
+            yhat_real = self.discriminator(real_images, training = True)
+            yhat_fake = self.discriminator(fake_images, training = True)
+            yhat_realfake = tf.concat([yhat_real, yhat_fake], axis = 0)
+            
+            #create lables for real and fakes images
+            #add label smoothing
+            y_realfake = tf.concat([0.9 * tf.ones_like(yhat_real), 0.1 * tf.zeros_like(yhat_fake)], axis = 0)
+
+            #calculate loss
+            total_d_loss = self.d_loss(y_realfake, yhat_realfake)
+
+            #apply backpropogation - nnlearn
+        dgrad = d_tape.gradient(total_d_loss, self.discriminator.trainable_variables)
+        self.d_opt.apply_gradients(zip(dgrad, self.discriminator.trainable_variables))
+
+        #Train the generator
+        with tf.GradientTape() as g_tape:
+            #Generate some new images
+            gen_images = self.generator(tf.random.normal((32, 100, 1)), training=True)
+            #Create the predicted labels
+            predicted_labels = self.discriminator(gen_images, training = False)
+            #Calculate loss
+            total_g_loss = self.g_loss(tf.ones_like(predicted_labels), predicted_labels)
+            
+        #Apply backprop
+        ggrad = g_tape.gradient(total_g_loss, self.generator.trainable_variables)
+        self.g_opt.apply_gradients(zip(ggrad, self.generator.trainable_variables))
+
+        return {"d_loss":total_d_loss, "g_loss": total_g_loss}
+
+
+def train_loop():
+    ds = load_dataset()
+    generator = build_generator()
+    #print(generator.summary())
+    discriminator = build_discriminator()
+    #print(discriminator.summary())
+    image_gen = image_generator(generator, discriminator)
+    image_gen.compile()
+    image_gen.fit(ds, epochs=10)
+    generator.save('generator.keras')
+    discriminator.save('discriminator.keras')    
+                   
+
+    generated_images = image_gen.generator(tf.random.normal((5, 100, 1)))
+    for i in range(generated_images.shape[0]):
+        plt.subplot(1, 5, i + 1)
+        plt.imshow(generated_images[i], cmap='gray')
+        plt.axis('off')
+    plt.show()
+    
+
+if __name__ == "__main__":
+    train_loop()
 
 def user_input():
     dataset, info = tfds.load('oxford_iiit_pet', with_info=True, as_supervised=True, split = 'train')
@@ -94,13 +163,4 @@ def user_input():
             print("valid input")
             pass
     print("Not a valid input")
-    return "Not a valid input"
-
-
-
-if __name__ == "__main__":
-    generator = build_generator()
-    print(generator.summary())
-    discrminator = build_discriminator()
-    print(discrminator.summary())
-    pass
+    return "Not a valid input"  
